9797e9574b70b49255def56fad880afeb4e16b2b
Santiago García <santiagogar2053@gmail.com>
2025-06-28 20:52:10 -0300

    se soluciona error en vfs-cat

--- src/vfs-cat.c
+++ src/vfs-cat.c
@@ -6,45 +6,49 @@
 #define BLOCK_SIZE 1024
 
 int main(int argc, char *argv[]) {
+    // estudiante1: comprobamos número de argumentos
     if (argc < 3) {
-        fprintf(stderr, "Uso: %s <vfs.img> <archivo1> [archivo2 ...]\n", argv[0]);
+        fprintf(stderr, "uso: %s <vfs.img> <archivo1> [archivo2 ...]\n", argv[0]);
         return 1;
     }
 
     const char *vfs_img = argv[1];
-    if (vfs_mount(vfs_img) != 0) {
-        fprintf(stderr, "No se pudo montar la imagen VFS: %s\n", vfs_img);
-        return 1;
-    }
-
+    // estudiante2: recorremos cada nombre de archivo pasado
     for (int i = 2; i < argc; i++) {
         const char *filename = argv[i];
-        int inode_num = dir_lookup(filename);
-
+        // estudiante1: buscamos el inode correspondiente al nombre
+        int inode_num = dir_lookup(vfs_img, filename);
         if (inode_num < 0) {
-            fprintf(stderr, "Archivo no encontrado: %s\n", filename);
+            fprintf(stderr, "archivo no encontrado: %s\n", filename);
             continue;
         }
 
         struct inode file_inode;
-        if (read_inode(inode_num, &file_inode) != 0) {
-            fprintf(stderr, "No se pudo leer el inode de: %s\n", filename);
+        // estudiante2: leemos metadata del inode
+        if (read_inode(vfs_img, inode_num, &file_inode) != 0) {
+            fprintf(stderr, "no se pudo leer el inode de: %s\n", filename);
             continue;
         }
 
+        // estudiante1: si está vacío, nada que mostrar
         if (file_inode.size == 0) {
-            // Archivo vacío, no imprime nada
             continue;
         }
 
         char buffer[BLOCK_SIZE];
-        int bytes_read = 0;
         int total_read = 0;
-        while (total_read < file_inode.size) {
+        // estudiante2: leemos en bloques hasta completar el tamaño
+        while (total_read < (int)file_inode.size) {
             int to_read = file_inode.size - total_read;
             if (to_read > BLOCK_SIZE) to_read = BLOCK_SIZE;
 
-            bytes_read = inode_read_data(&file_inode, total_read, buffer, to_read);
+            int bytes_read = inode_read_data(
+                vfs_img,
+                inode_num,
+                buffer,
+                to_read,
+                total_read
+            );
             if (bytes_read <= 0) break;
 
             fwrite(buffer, 1, bytes_read, stdout);
@@ -52,5 +56,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    vfs_umount();
+    // estudiante1: fin del programa
     return 0;
+}
