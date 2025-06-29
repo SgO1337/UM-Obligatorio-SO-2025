e77b49748400ded4536b74397d629ad1b3512dcc
buenahora <buenahorafranco@gmail.com>
2025-06-28 21:42:40 -0300

    Cambio en comentarios

--- src/vfs-cat.c
+++ src/vfs-cat.c
@@ -6,17 +6,17 @@
 #define BLOCK_SIZE 1024
 
 int main(int argc, char *argv[]) {
-    // estudiante1: comprobamos número de argumentos
+    //comprobamos número de argumentos
     if (argc < 3) {
         fprintf(stderr, "uso: %s <vfs.img> <archivo1> [archivo2 ...]\n", argv[0]);
         return 1;
     }
 
     const char *vfs_img = argv[1];
-    // estudiante2: recorremos cada nombre de archivo pasado
+    //recorremos cada nombre de archivo pasado
     for (int i = 2; i < argc; i++) {
         const char *filename = argv[i];
-        // estudiante1: buscamos el inode correspondiente al nombre
+        //buscamos el inode correspondiente al nombre
         int inode_num = dir_lookup(vfs_img, filename);
         if (inode_num < 0) {
             fprintf(stderr, "archivo no encontrado: %s\n", filename);
@@ -24,20 +24,20 @@ int main(int argc, char *argv[]) {
         }
 
         struct inode file_inode;
-        // estudiante2: leemos metadata del inode
+        //leemos metadata del inode
         if (read_inode(vfs_img, inode_num, &file_inode) != 0) {
             fprintf(stderr, "no se pudo leer el inode de: %s\n", filename);
             continue;
         }
 
-        // estudiante1: si está vacío, nada que mostrar
+        //si está vacío, nada que mostrar
         if (file_inode.size == 0) {
             continue;
         }
 
         char buffer[BLOCK_SIZE];
         int total_read = 0;
-        // estudiante2: leemos en bloques hasta completar el tamaño
+        //leemos en bloques hasta completar el tamaño
         while (total_read < (int)file_inode.size) {
             int to_read = file_inode.size - total_read;
             if (to_read > BLOCK_SIZE) to_read = BLOCK_SIZE;
